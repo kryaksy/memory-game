4594404ab2b7e6c5b6782848ddc04c25b1f60655
kryaksy <ib.koray@gmail.com>
2018-03-22 18:02:16 +0300

    Added game initialization function

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 /*
+ * Dom Elements
+ */
+const gameBoard = document.querySelector('.game-board')
+
+/*
  * Create a list that holds all of your cards
  */
  let faItems = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
@@ -10,6 +15,26 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+
+ // Creating Deck HTML
+ function newGame(a) {
+    shuffle(faItems);
+    var newDeck = document.createElement('container');
+    newDeck.classList.add('deck');
+
+	for (var i = 0; i < faItems.length; i++) {
+		var newCard = document.createElement('div');
+        var back = document.createElement('div');
+        newCard.classList.add('card');
+        newCard.id = i + 1;
+		back.classList.add('back', 'fa', faItems[i]);
+		newCard.append(back);
+        newDeck.append(newCard);
+	}
+
+    a.append(newDeck);
+}
 
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -38,3 +63,4 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ newGame(gameBoard);
