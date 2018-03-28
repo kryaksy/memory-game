d7805cfcb5fc9d2f60720acb421c4614d0490b10
İbrahim Aksoy <ibo@Ibrahim-MacBook-Pro.local>
2018-03-28 03:38:21 +0300

    Minimized codes in shuffle function

--- js/app.js
+++ js/app.js
@@ -30,14 +30,14 @@ function newGame() {
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var curr = array.length, temp, rand;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    while (curr) {
+        rand = Math.floor(Math.random() * curr);
+        curr--;
+        temp = array[curr];
+        array[curr] = array[rand];
+        array[rand] = temp;
     }
     return array;
 }
